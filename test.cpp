0373fbc5d4e0c9a1d792011eafc3a9597e0984fc
你çtjy <tanyi13333@163.com>
2025-12-05 20:06:29 +0800

    提交test.cpp

---
+++ test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 3, b = 5;
+    cout << "Hello from C++!" << endl;
+    cout << "3 + 5 = " << a + b << endl;
+    return 0;
+}
+
